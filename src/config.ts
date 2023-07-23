e8dd0febeaa3b86406907ab6092d4e7933eeebd4
huliang <huliang0222@gmail.com>
2023-07-23 21:41:59 +0800

    feat: add sample code

---
+++ src/config.ts
@@ -0,0 +1,50 @@
+import { Token } from "@uniswap/sdk-core";
+import { FeeAmount } from "@uniswap/v3-sdk";
+
+import { USDC_TOKEN, WETH_TOKEN } from "./libs/constants";
+
+// Sets if the example should run locally or on chain
+export enum Environment {
+  LOCAL,
+  MAINNET,
+  WALLET_EXTENSION,
+}
+
+// Inputs that configure this example to run
+export interface ExampleConfig {
+  env: Environment;
+  rpc: {
+    local: string;
+    mainnet: string;
+  };
+  wallet: {
+    address: string;
+    privateKey: string;
+  };
+  tokens: {
+    in: Token;
+    amountIn: number;
+    out: Token;
+    poolFee: number;
+  };
+}
+
+// Example Configuration
+
+export const CurrentConfig: ExampleConfig = {
+  env: Environment.WALLET_EXTENSION,
+  rpc: {
+    local: "http://localhost:8545",
+    mainnet: "https://goerli.infura.io/v3/929ca938c72f43c887598d1a34dc7d00",
+  },
+  wallet: {
+    address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+    privateKey: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  },
+  tokens: {
+    in: WETH_TOKEN,
+    amountIn: 0.001,
+    out: USDC_TOKEN,
+    poolFee: FeeAmount.MEDIUM,
+  },
+};
